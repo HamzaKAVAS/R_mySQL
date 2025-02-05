a56624a2af4f033a7cb3e6b7ab6f06bc92599a9e
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-05 18:14:37 +0300

    V1

--- HamzaPractice.sql
+++ HamzaPractice.sql
@@ -1,3 +1,15 @@
 create schema HamzaDeneme;
 use HamzaDeneme;
 
+CREATE TABLE takim (
+ personel_id INT,
+ adi VARCHAR(40),
+ soyadi VARCHAR(40),
+ dogum_tarihi DATE,
+ pozisyon VARCHAR(40),
+ maas INT
+ );
+ 
+ select * from takim;
+
+
