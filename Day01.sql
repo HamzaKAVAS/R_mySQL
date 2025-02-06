a1e0ab20ab978384c6465aa6abaa74f0d8827684
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-06 21:49:22 +0300

    V2

--- Day01.sql
+++ Day01.sql
@@ -38,6 +38,45 @@ alter table ogrenciler DROP COLUMN ogrenci_yasi;
 -- ogrenciler tablosunu silin
 DROP TABLE ogrenciler;
 
+-- ===================================================================================================================================
+
+CREATE TABLE kisiler
+(
+isim VARCHAR(30),
+soyisim VARCHAR(30),
+yas INT,
+sehir VARCHAR(20)
+);
+
+INSERT INTO kisiler () VALUES ('Ahmet', 'Yılmaz', 30, 'Ankara');
+INSERT INTO kisiler () VALUES ('Mehmet', 'Kara', 25, 'İstanbul');
+INSERT INTO kisiler () VALUES ('Ayşe', 'Kaya', 22, 'İzmir');
+INSERT INTO kisiler () VALUES ('Fatma', 'Güneş', 28, 'Bursa');
+INSERT INTO kisiler () VALUES ('Ali', 'Çelik', 35, 'Antalya');
+INSERT INTO kisiler () VALUES ('Elif', 'Demir', 29, 'Ankara');
+INSERT INTO kisiler () VALUES ('Can', 'Özdemir', 33, 'İstanbul');
+INSERT INTO kisiler () VALUES ('Deniz', 'Arslan', 24, 'İzmir');
+INSERT INTO kisiler () VALUES ('Selin', 'Yıldız', 27, 'Bursa');
+INSERT INTO kisiler () VALUES ('Emre', 'Koç', 31, 'Antalya');
+INSERT INTO kisiler () VALUES ('Zeynep', 'Çınar', 26, 'Konya');
+INSERT INTO kisiler () VALUES ('Oğuz', 'Kaplan', 40, 'Samsun');
+INSERT INTO kisiler () VALUES ('Burcu', 'Ertuğrul', 22, 'Trabzon');
+INSERT INTO kisiler () VALUES ('Kaya', 'Durmaz', 35, 'Diyarbakır');
+INSERT INTO kisiler () VALUES ('Seda', 'Aydın', 28, 'Eskişehir');
+
+select * from kisiler;
+select isim,soyisim from kisiler where sehir='bursa';
+
+/*
+========================  SORU-1 ========================
+ Kisiler tablosundan isim ve soy isim bilgilerini listeleyiniz.
+=========================================================
+ */
+ 
+ select isim,soyisim from kisiler;
+
+ 
+
 								                                 
 
 
