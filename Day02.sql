d1ec8a6b39201362595fe69ff7919121bbf9147f
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-07 21:50:55 +0300

    V3

--- Day02.sql
+++ Day02.sql
@@ -4,21 +4,33 @@ use HamzaDeneme;
 SELECT * FROM kelimeler;
 
 -- kelimeler tablosunda harf sayısı 3'den büyük olanları listeleyin
-SELECT * FROM kelimeler WHERE harf_sayisi > 3;
+SELECT * FROM kelimeler 
+WHERE harf_sayisi > 3;
 
 -- kelimeler tablosunda harf sayısı 3'den fazla olan kelimelerin id'lerini listeleyin
-SELECT id FROM kelimeler WHERE harf_sayisi > 3;
+SELECT id FROM kelimeler 
+WHERE harf_sayisi > 3;
 
 -- kelimeler tablosunda harf sayısı 1'den fazla ve 5'den az olan kelimeleri listeleyin
-SELECT kelime FROM kelimeler WHERE harf_sayisi BETWEEN 2 AND 4;
+SELECT kelime FROM kelimeler
+WHERE harf_sayisi BETWEEN 2 AND 4;
 
 -- 2.harfi o olan kelimeleri listeleyin
-SELECT kelime FROM kelimeler WHERE kelime LIKE '_o%';
+SELECT kelime FROM kelimeler 
+WHERE kelime LIKE '_o%';
 
 -- ikinci harfi o olan ve uzunluğu 3 olan kelimeleri listeleyin
-SELECT kelime FROM kelimeler WHERE kelime LIKE '_o%' AND harf_sayisi=3;
-SELECT kelime FROM kelimeler WHERE kelime LIKE '_o_';
-SELECT kelime FROM kelimeler WHERE REGEXP_LIKE (kelime,'^.o.$');
+SELECT kelime FROM kelimeler 
+WHERE kelime LIKE '_o%' AND harf_sayisi=3;
+
+SELECT kelime FROM kelimeler 
+WHERE kelime LIKE '_o_';
+
+SELECT kelime FROM kelimeler 
+WHERE REGEXP_LIKE (kelime,'^.o.$');
 
 -- ilk harfi h olup kelime uzunluğu 3'den fazla olan kelimeleri listeleyin
-SELECT kelime FROM kelimeler WHERE kelime LIKE 'h%' AND harf_sayisi>3;
+SELECT kelime FROM kelimeler
+WHERE kelime LIKE 'h%' AND harf_sayisi>3;
+
+-- ================================================================================================
