99b82cae4a49e5977be55ef91c22204d04bdd278
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-07 22:16:45 +0300

    V5

--- Day02.sql
+++ Day02.sql
@@ -55,5 +55,68 @@ VALUES  (1, 'Savaş ve Barış',    'Lev Tolstoy',  1869,   'Tarihi' ),
         (9, 'Faust',    'Johann Wolfgang von Goethe',   1808,   'Felsefi'),
         (10,    'Ulysses',  'James Joyce',  1922,   'Modernist');
         
-SELECT * FROM kitaplar;       
+SELECT * FROM kitaplar;
+
+-- tüm kitapları tekrarsız olarak listeleyin
+SELECT DISTINCT * FROM kitaplar;        
+SELECT DISTINCT kitap_adi FROM kitaplar;    
+
+-- listedeki yazarları tekrarsız olarak listeleyin.
+SELECT DISTINCT yazar_adi FROM kitaplar;    
+
+-- 1860 ile 1890 yılları arasında kitap yayınlayan yazarları TEKRARSIZ olarak listeleyin
+SELECT DISTINCT yazar_adi FROM kitaplar
+WHERE yayin_yili BETWEEN 1860 AND 1890;
+
+-- ================================================================================================
+
+CREATE TABLE kitaplik (
+    id INT,
+    kitap_adi VARCHAR(100),
+    yazar VARCHAR(50),
+    yayin_yili INT
+);
+
+INSERT INTO kitaplik (id, kitap_adi, yazar, yayin_yili) VALUES
+(1, 'Sefiller', 'Victor Hugo', 1862),
+(2, '1984', 'George Orwell', 1949),
+(3, 'Suç ve Ceza', 'Fyodor Dostoyevski', 1866),
+(4, 'Kürk Mantolu Madonna', 'Sabahattin Ali', 1943),
+(5, 'Yabancı', 'Albert Camus', 1942),
+(6, 'Ulysses', 'James Joyce', 1922),
+(7, 'Yüzüklerin Efendisi', 'J.R.R. Tolkien', 1954),
+(8, 'Harry Potter ve Felsefe Taşı', 'J.K. Rowling', 1998),
+(9, 'Da Vinci Şifresi', 'Dan Brown', 2003),
+(10, 'Zamanın Kısa Tarihi', 'Stephen Hawking', 1998),
+(11, 'Karamazov Kardeşler', 'Fyodor Dostoyevski', 1880),
+(12, 'Budala', 'Fyodor Dostoyevski', 1869),
+(13, 'Savaş ve Barış', 'Lev Tolstoy', 1866),
+(14, 'Anna Karenina', 'Lev Tolstoy', 1877),
+(15, 'Hobbit', 'J.R.R. Tolkien', 1942);
+
+SELECT * FROM kitaplik;
+
+/*  
+============================= SORU-1 =============================
+    Kitaplik tablosundaki kitapları yayın yılına göre eski olanlardan
+    yeni olanlara doğru sıralayiniz.
+===================================================================
+*/
+
+SELECT kitap_adi,yayin_yili FROM kitaplik
+ORDER BY yayin_yili ASC;
+
+SELECT kitap_adi,yayin_yili FROM kitaplik
+ORDER BY yayin_yili;
+
+/*
+============================= SORU-2 =============================
+    Kitaplik tablosundaki kitapların yayın yılına göre yeni olanlardan
+    eski olanlara doğru sıralayiniz.
+===================================================================
+*/
+
+SELECT kitap_adi,yayin_yili FROM kitaplik
+ORDER BY yayin_yili DESC;
+
 
