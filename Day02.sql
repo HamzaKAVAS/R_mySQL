f9b94b33a59fd60fba46669f006f0b2d0bfda92e
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-07 22:02:17 +0300

    V4

--- Day02.sql
+++ Day02.sql
@@ -34,3 +34,26 @@ SELECT kelime FROM kelimeler
 WHERE kelime LIKE 'h%' AND harf_sayisi>3;
 
 -- ================================================================================================
+
+CREATE TABLE kitaplar (
+kitap_id  INT,
+kitap_adi VARCHAR(50),
+yazar_adi VARCHAR(50),
+yayin_yili INT,
+kategori VARCHAR(30) 
+);
+
+INSERT INTO kitaplar ()
+VALUES  (1, 'Savaş ve Barış',    'Lev Tolstoy',  1869,   'Tarihi' ),
+        (2, 'Suç ve Ceza', 'Fyodor Dostoyevski',   1866,   'Psikolojik'),
+        (3, 'Anna Karenina',    'Lev Tolstoy',  1877,   'Aşk'),
+        (4, '1984', 'George Orwell',    1949,   'Politik'),
+        (5, 'Büyük Umutlar',  'Charles Dickens',  1861,   'Dram'),
+        (6, 'Don Kişot',   'Miguel de Cervantes',  1605,   'Macera'),
+        (7, 'Gurur ve Önyargı',   'Jane Austen',  1813,   'Dram'),
+        (8, 'Monte Kristo Kontu',   'Alexandre Dumas',  1844,   'Macera'),
+        (9, 'Faust',    'Johann Wolfgang von Goethe',   1808,   'Felsefi'),
+        (10,    'Ulysses',  'James Joyce',  1922,   'Modernist');
+        
+SELECT * FROM kitaplar;       
+
