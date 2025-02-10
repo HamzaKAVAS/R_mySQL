d39ca410a26906390fb607bfe04655e3e58c30d9
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-10 17:32:18 +0300

    V2

--- Day03.sql
+++ Day03.sql
@@ -11,3 +11,35 @@ WHERE harf_sayisi > 3;
 SELECT id FROM kelimeler
 WHERE harf_sayisi > 3;
 
+-- kelimeler tablosunda harf sayısı 1'den fazla ve 5'den az olan kelimeleri listeleyin
+SELECT kelime FROM kelimeler
+WHERE harf_sayisi BETWEEN 2 AND 4;
+
+-- 2.harfi o olan kelimeleri listeleyin
+SELECT kelime FROM kelimeler
+WHERE kelime LIKE '_o%';
+
+-- ikinci harfi o olan ve uzunluğu 3 olan kelimeleri listeleyin
+SELECT kelime FROM kelimeler
+WHERE kelime LIKE '_o_';
+-- ya da
+SELECT kelime FROM kelimeler
+WHERE REGEXP_LIKE (kelime,'^.o.$');
+
+-- ilk harfi h olup kelime uzunluğu 3'den fazla olan kelimeleri listeleyin
+SELECT kelime FROM kelimeler
+WHERE kelime LIKE 'h%' AND harf_sayisi > 3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
