eb1d798409bd430f18292a67e021f4c92d1d1b80
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-12 21:33:33 +0300

    V2

--- Day05.sql
+++ Day05.sql
@@ -1,1 +1,36 @@
 use hamzadeneme;
+
+CREATE TABLE calisanlar(
+id int,
+isim VARCHAR(50),
+sehir VARCHAR(50),
+maas int,
+sirket VARCHAR(20)
+);
+
+INSERT INTO calisanlar VALUES(123456789, 'Ali Seker', 'Istanbul', 2500, 'Honda');
+INSERT INTO calisanlar VALUES(234567890, 'Ayse Gul', 'Istanbul', 1500, 'Toyota');
+INSERT INTO calisanlar VALUES(345678901, 'Veli Yilmaz', 'Ankara', 3000, 'Honda');
+INSERT INTO calisanlar VALUES(456789012, 'Veli Yilmaz', 'Izmir', 1000, 'Ford');
+INSERT INTO calisanlar VALUES(567890123, 'Veli Yilmaz', 'Ankara', 7000, 'Hyundai');
+INSERT INTO calisanlar VALUES(456789012, 'Ayse Gul', 'Ankara', 1500, 'Ford');
+INSERT INTO calisanlar VALUES(123456710, 'Fatma Yasa', 'Bursa', 2500, 'Honda');
+
+CREATE TABLE sirketler(
+sirket_id int,
+sirket VARCHAR(20),
+calisanlar_sayisi int
+);
+
+INSERT INTO sirketler VALUES(100, 'Honda', 12000);
+INSERT INTO sirketler VALUES(101, 'Ford', 18000);
+INSERT INTO sirketler VALUES(102, 'Hyundai', 10000);
+INSERT INTO sirketler VALUES(103, 'Toyota', 21000);
+
+SELECT * FROM calisanlar;
+SELECT * FROM sirketler;
+
+
+
+
+
