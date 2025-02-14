c13a7c6e866d461fbab7c244ac7acc05e5c4974b
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-14 20:08:49 +0300

    V7

--- Day05.sql
+++ Day05.sql
@@ -290,6 +290,43 @@ WHERE urun_id = 1001;
 INSERT INTO tedarik() VALUES (104,'HUAWEI','Mehmet Can');
 INSERT INTO urun() VALUES (104,1005,'HUAWEI','Rüya Koca');
 
+CREATE TABLE cocuklar(
+id INT,
+isim VARCHAR(20),
+veli_isim VARCHAR(10),
+grade DOUBLE,
+CONSTRAINT id_pk PRIMARY KEY (id)
+);
+
+INSERT INTO cocuklar VALUES
+ (123, 'Ali Can', 'Hasan',75), 
+ (124, 'Merve Gul', 'Ayse',85), 
+ (125, 'Kemal Yasa', 'Hasan',85),
+ (126, 'Rumeysa Aydin', 'Zeynep',85),
+ (127, 'Oguz Karaca', 'Tuncay',85),
+ (128, 'Resul Can', 'Tugay',85),
+ (129, 'Tugay Kala', 'Osman',45);
+ 
+CREATE TABLE puanlar(
+ogrenci_id INT,
+ders_adi VARCHAR(10),
+yazili_notu DOUBLE,
+CONSTRAINT puanlar_fk 
+FOREIGN KEY (ogrenci_id) 
+REFERENCES cocuklar (id)
+);
+
+INSERT INTO puanlar VALUES ('123','kimya',75); 
+INSERT INTO puanlar VALUES ('124','fizik',65); 
+INSERT INTO puanlar VALUES ('125','tarih',90); 
+INSERT INTO puanlar VALUES ('126','kimya',87); 
+INSERT INTO puanlar VALUES ('127','tarih',69); 
+INSERT INTO puanlar VALUES ('128','kimya',93); 
+INSERT INTO puanlar VALUES ('129','fizik',25); 
+
+SELECT * FROM cocuklar;
+SELECT * FROM puanlar;
+
 
 
 
