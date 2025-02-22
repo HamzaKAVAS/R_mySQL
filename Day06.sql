afcea902eb13d0a1508d9dd154d04ae62775fe22
HamzaKAVAS <hmzkavas@gmail.com>
2025-02-22 21:36:10 +0300

    New

--- Day06.sql
+++ Day06.sql
@@ -220,6 +220,9 @@ ON sip.sirket_id = sir.sirket_id;
   ORNEK2: sirket tablosundaki tum sirketleri ve bu sirketlere ait olan 
   siparis_id ve siparis_tarihleri listeleyen bir sorgu yaziniz.
 ------------------------------------------------------------------------------*/
+SELECT sir.sirket_isim,sip.siparis_id,sip.siparis_tarihi
+FROM sirket sir LEFT JOIN siparisler sip
+ON sip.sirket_id = sir.sirket_id;
 
 
 
